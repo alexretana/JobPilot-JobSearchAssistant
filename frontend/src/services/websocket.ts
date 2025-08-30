a173aaab854070b957a6cc45620171602871b1ce
alexretana <alex.retana@live.com>
2025-08-30 12:45:17 -0400

    Begin playwright validation

--- frontend/src/services/websocket.ts
+++ frontend/src/services/websocket.ts
@@ -1,15 +1,38 @@
 // Simple WebSocket service mock
-export const webSocketService = {
-  connect: () => {
-    console.log('WebSocket connected');
-  },
-  disconnect: () => {
-    console.log('WebSocket disconnected');
-  },
-  sendMessage: (message: any) => {
-    console.log('WebSocket message sent:', message);
-  },
-  onMessage: (callback: (message: any) => void) => {
-    console.log('WebSocket message handler registered');
-  }
-};
+export const webSocketService = (() => {
+  let isConnected = false;
+  let messageHandlers: Array<(message: any) => void> = [];
+  
+  return {
+    connect: () => {
+      console.log('WebSocket connected');
+      isConnected = true;
+    },
+    disconnect: () => {
+      console.log('WebSocket disconnected');
+      isConnected = false;
+    },
+    sendMessage: (message: any) => {
+      console.log('WebSocket message sent:', message);
+    },
+    onMessage: (callback: (message: any) => void) => {
+      console.log('WebSocket message handler registered');
+    },
+    getIsConnected: () => {
+      return () => isConnected;
+    },
+    addMessageHandler: (handler: (message: any) => void) => {
+      messageHandlers.push(handler);
+      console.log('Message handler added');
+      
+      // Return a function to remove the handler
+      return () => {
+        const index = messageHandlers.indexOf(handler);
+        if (index > -1) {
+          messageHandlers.splice(index, 1);
+          console.log('Message handler removed');
+        }
+      };
+    }
+  };
+})();
